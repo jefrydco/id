cc0608a662d05e78daa5198f1794c88f6b97e3b9
Jefry Dewangga <jefrydco@gmail.com>
2026-01-13 11:47:59 +0700

    feat: restructure the llms.txt

--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -5,12 +5,15 @@ import { getTalkSlug, getSortedTalksByLocale } from "@/utils/i18n-talk-content";
 import { getNoteSlug, getSortedNotesByLocale } from "@/utils/i18n-notes";
 
 export const GET: APIRoute = async () => {
-	const posts = await getSortedPostsByLocale("en");
-	const talks = await getSortedTalksByLocale("en");
-	const notes = await getSortedNotesByLocale("en");
+	const postsEn = await getSortedPostsByLocale("en");
+	const postsId = await getSortedPostsByLocale("id");
+	const talksEn = await getSortedTalksByLocale("en");
+	const talksId = await getSortedTalksByLocale("id");
+	const notesEn = await getSortedNotesByLocale("en");
+	const notesId = await getSortedNotesByLocale("id");
 	const baseUrl = themeConfig.site.website.replace(/\/$/, "");
 
-	const blogPostsSection = posts
+	const blogPostsEnSection = postsEn
 		.map((post) => {
 			const slug = getPostSlug(post);
 			const description = post.data.description
@@ -20,7 +23,17 @@ export const GET: APIRoute = async () => {
 		})
 		.join("\n");
 
-	const talksSection = talks
+	const blogPostsIdSection = postsId
+		.map((post) => {
+			const slug = getPostSlug(post);
+			const description = post.data.description
+				? `: ${post.data.description}`
+				: "";
+			return `- [${post.data.title}](${baseUrl}/id/blog/${slug}.md)${description}`;
+		})
+		.join("\n");
+
+	const talksEnSection = talksEn
 		.map((talk) => {
 			const slug = getTalkSlug(talk);
 			const description = talk.data.description
@@ -30,7 +43,17 @@ export const GET: APIRoute = async () => {
 		})
 		.join("\n");
 
-	const notesSection = notes
+	const talksIdSection = talksId
+		.map((talk) => {
+			const slug = getTalkSlug(talk);
+			const description = talk.data.description
+				? `: ${talk.data.description}`
+				: "";
+			return `- [${talk.data.title}](${baseUrl}/id/talk/${slug}.md)${description}`;
+		})
+		.join("\n");
+
+	const notesEnSection = notesEn
 		.map((note) => {
 			const slug = getNoteSlug(note);
 			const description = note.data.description
@@ -40,22 +63,57 @@ export const GET: APIRoute = async () => {
 		})
 		.join("\n");
 
-	const blogTags = [...new Set(posts.flatMap((post) => post.data.tags))].sort();
-	const talkTags = [...new Set(talks.flatMap((talk) => talk.data.tags))].sort();
-	const notesTags = [
-		...new Set(notes.flatMap((note) => note.data.tags)),
+	const notesIdSection = notesId
+		.map((note) => {
+			const slug = getNoteSlug(note);
+			const description = note.data.description
+				? `: ${note.data.description}`
+				: "";
+			return `- [${note.data.title}](${baseUrl}/id/notes/${slug}.md)${description}`;
+		})
+		.join("\n");
+
+	const blogTagsEn = [
+		...new Set(postsEn.flatMap((post) => post.data.tags)),
+	].sort();
+	const blogTagsId = [
+		...new Set(postsId.flatMap((post) => post.data.tags)),
+	].sort();
+	const talkTagsEn = [
+		...new Set(talksEn.flatMap((talk) => talk.data.tags)),
+	].sort();
+	const talkTagsId = [
+		...new Set(talksId.flatMap((talk) => talk.data.tags)),
+	].sort();
+	const notesTagsEn = [
+		...new Set(notesEn.flatMap((note) => note.data.tags)),
+	].sort();
+	const notesTagsId = [
+		...new Set(notesId.flatMap((note) => note.data.tags)),
 	].sort();
 
-	const blogTagsSection = blogTags
+	const blogTagsEnSection = blogTagsEn
 		.map((tag) => `- [${tag}](${baseUrl}/blog/tags/${tag}/)`)
 		.join("\n");
 
-	const talkTagsSection = talkTags
+	const blogTagsIdSection = blogTagsId
+		.map((tag) => `- [${tag}](${baseUrl}/id/blog/tags/${tag}/)`)
+		.join("\n");
+
+	const talkTagsEnSection = talkTagsEn
 		.map((tag) => `- [${tag}](${baseUrl}/talk/tags/${tag}/)`)
 		.join("\n");
 
-	const notesTagsSection = notesTags
+	const talkTagsIdSection = talkTagsId
+		.map((tag) => `- [${tag}](${baseUrl}/id/talk/tags/${tag}/)`)
+		.join("\n");
+
+	const notesTagsEnSection = notesTagsEn
 		.map((tag) => `- [${tag}](${baseUrl}/notes/tags/${tag}/)`)
+		.join("\n");
+
+	const notesTagsIdSection = notesTagsId
+		.map((tag) => `- [${tag}](${baseUrl}/id/notes/tags/${tag}/)`)
 		.join("\n");
 
 	const content = `# ${themeConfig.site.title}
@@ -70,47 +128,90 @@ export const GET: APIRoute = async () => {
 - [Notes](${baseUrl}/notes/): Quick notes and tips
 - [Search](${baseUrl}/search/): Full-text search
 
-## Blog Posts (Markdown)
+## Blog Posts
 
-${blogPostsSection}
+### English
 
-## Talks (Markdown)
+${blogPostsEnSection}
 
-${talksSection}
+### Indonesian
 
-## Notes (Markdown)
+${blogPostsIdSection}
 
-${notesSection}
+## Talks
+
+### English
+
+${talksEnSection}
+
+### Indonesian
+
+${talksIdSection}
+
+## Notes
+
+### English
+
+${notesEnSection}
+
+### Indonesian
+
+${notesIdSection}
 
 ## Tags
 
 ### Blog Tags
 
-${blogTagsSection}
+#### English
+
+${blogTagsEnSection}
+
+#### Indonesian
+
+${blogTagsIdSection}
 
 ### Talk Tags
 
-${talkTagsSection}
+#### English
+
+${talkTagsEnSection}
+
+#### Indonesian
+
+${talkTagsIdSection}
 
 ### Notes Tags
 
-${notesTagsSection}
+#### English
+
+${notesTagsEnSection}
+
+#### Indonesian
+
+${notesTagsIdSection}
 
 ## Feeds
 
-- [Blog RSS Feed](${baseUrl}/blog/rss.xml): Blog RSS 2.0 feed
-- [Blog Atom Feed](${baseUrl}/blog/atom.xml): Blog Atom feed
-- [Talks RSS Feed](${baseUrl}/talk/rss.xml): Talks RSS 2.0 feed
-- [Talks Atom Feed](${baseUrl}/talk/atom.xml): Talks Atom feed
-- [Notes RSS Feed](${baseUrl}/notes/rss.xml): Notes RSS 2.0 feed
-- [Notes Atom Feed](${baseUrl}/notes/atom.xml): Notes Atom feed
+### Blog
 
-## Optional
+- [RSS](${baseUrl}/blog/rss.xml)
+- [Atom](${baseUrl}/blog/atom.xml)
+- [RSS Indonesian](${baseUrl}/id/blog/rss.xml)
+- [Atom Indonesian](${baseUrl}/id/blog/atom.xml)
 
-- [Indonesian Home](${baseUrl}/id/): Indonesian version
-- [Indonesian Blog](${baseUrl}/id/blog/): Indonesian blog posts
-- [Indonesian Talks](${baseUrl}/id/talk/): Indonesian talks
-- [Indonesian Notes](${baseUrl}/id/notes/): Indonesian notes
+### Talks
+
+- [RSS](${baseUrl}/talk/rss.xml)
+- [Atom](${baseUrl}/talk/atom.xml)
+- [RSS Indonesian](${baseUrl}/id/talk/rss.xml)
+- [Atom Indonesian](${baseUrl}/id/talk/atom.xml)
+
+### Notes
+
+- [RSS](${baseUrl}/notes/rss.xml)
+- [Atom](${baseUrl}/notes/atom.xml)
+- [RSS Indonesian](${baseUrl}/id/notes/rss.xml)
+- [Atom Indonesian](${baseUrl}/id/notes/atom.xml)
 `;
 
 	return new Response(content, {
